628ac82ab2cc650796b30567e58bd19b830e14a6
victor <victor.murithi@student.moringaschool.com>
2023-11-23 11:08:05 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return "[]\n"
+    
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    # Print the Fibonacci sequence in the expected format
+    print("Fibonacci sequence:", fibonacci_sequence)
+
+    return fibonacci_sequence
+    # rest of the function implementation
